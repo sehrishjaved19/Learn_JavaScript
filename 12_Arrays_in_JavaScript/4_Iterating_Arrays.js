c2e830f50d7e1994a6fc0a6cfedbec26fc93dcff
Sehrish <sehrishjaved119@gmail.com>
2025-10-08 21:57:46 +0500

    Added

--- 12_Arrays_in_JavaScript/4_Iterating_Arrays.js
+++ 12_Arrays_in_JavaScript/4_Iterating_Arrays.js
@@ -4,21 +4,46 @@ for (let i = 0; i<fruits.length; i++) {
     console.log(fruits[i]);
 }
 
-// Array Iterating (for... of loop)
-for (let fruit of fruits) {
-  console.log(fruit);
+// forEach method
+//Sends a welcome email to each user
+
+function sendWelcomeEmail (email) {
+  console.log(`Welcome email sent to ${email}`);
 }
-
-// Array Iterating (for...in loop)
-
-for (let index in fruits) {
-  console.log(index, fruits[index]);
-}
-
-// Array Iterating (forEach loop)
-fruits.forEach(function(fruit) {
-  console.log(fruit);
+const user = [
+  { name: 'Alice', email: 'alice@example.com'},
+  { name: 'Bob', email: 'bob@example.com'},
+  { name: 'Charlie', email: 'charlie@example.com'}
+];
+user.forEach ((user) => {
+  sendWelcomeEmail(user.email);
 });
 
-//Using Arrow Function(forEach loop)
-fruits.forEach(fruit => console.log(fruit));
+// Map Method
+
+const products = [
+  { name: 'Laptop', price: 1000 },
+  { name: 'Smartphone', price: 500 },
+  { name: 'Tablet', price: 300 }
+];
+products.map((product) => {
+  console.log(`The price of ${product.name} is ${product.price}`);
+});//map method construst and log messages for each product
+
+//Filter method
+const Products = [
+  { name: 'Laptop' , price: 1000 },
+  { name: 'Smartphone' , price:500 },
+  { name: 'Tablet' , price: 300 },
+  { name: 'Monitor' , price: 250 },
+  { name: 'Keyboard' , price: 50 }
+];
+function filterProductByPriceRange (Products, minPrice, maxPrice) {
+  return Products.filter((product) => product.price >= minPrice && product.price <= maxPrice);
+}
+const minPrice = 100;
+const maxPrice = 500;
+const filteredProducts = filterProductByPriceRange(Products , minPrice, maxPrice);
+filteredProducts.forEach((product) => {
+  console.log(`${product.name} is of ${product.price}`);
+});
